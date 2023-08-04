b97eeaafc2b4cb25fb2b2f30da2d3f25bd231313
sasami <sasami2008github@outlook.com>
2023-08-04 12:10:34 +0800

    增加远程安装vscode server脚本

---
+++ powershell/uploadVSCodeServer.ps1
@@ -0,0 +1,31 @@
+# 可以在host没有网络的条件下上传vscode server 安装包进行远程安装
+# ============================================================
+# 客户端vscode版本,从 Help->About 中 copy 过来
+
+$version = "2ccd690cbff1569e4a83d7c43d45101f817401dc" 
+$hostName = "calvin"
+$hostPort = "22"
+$hostIp = "192.168.1.100"
+
+# ============================================================
+$downFileName = "$version.tar.gz"
+$vscServerDir = "vscs"
+$downFileDir = "$ENV:TMP/$vscServerDir"
+$downFile = "$downFileDir/$downFileName"
+
+md $downFileDir
+
+$client = [System.Net.WebClient]::new()
+$client.DownloadFile("https://update.code.visualstudio.com/commit:$version/server-linux-x64/stable","$downFile ")
+
+
+$hostAddress = "$hostName@$hostIp"
+$hostHome = "/home/$hostName"
+$hostVSCDir = "$hostHome/$vscServerDir"
+$hostInstallPath = "$hostHome/.vscode-server/bin/$version"
+
+scp -r -P $hostPort $downFileDir $hostAddress`:$hostHome 
+
+$hostInstallSh = "cd $hostVSCDir ; tar -zxf $downFileName ; rm -rf $downFileName ; mv vscode-server-linux-x64/ $hostInstallPath/"
+
+ssh -p $hostPort $hostAddress $hostInstallSh
