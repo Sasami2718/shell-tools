d5b2fcf8f4250b2756a7325505426cc53aa706cf
sasami <sasami2008github@outlook.com>
2023-08-10 18:55:20 +0800

    1. 增加清空本地下载缓存文件夹的操作; 2. 增加host安装前清空临时安装目录操作

--- powershell/uploadVSCodeServer.ps1
+++ powershell/uploadVSCodeServer.ps1
@@ -13,7 +13,9 @@ $vscServerDir = "vscs"
 $downFileDir = "$ENV:TMP/$vscServerDir"
 $downFile = "$downFileDir/$downFileName"
 
-md $downFileDir
+rd $downFileDir -Recurse # 先清空vscserver缓存文件夹
+
+md $downFileDir # 创建vscserver 缓存文件夹
 
 $client = [System.Net.WebClient]::new()
 $client.DownloadFile("https://update.code.visualstudio.com/commit:$version/server-linux-x64/stable","$downFile ")
@@ -26,6 +28,6 @@ $hostInstallPath = "$hostHome/.vscode-server/bin/$version"
 
 scp -r -P $hostPort $downFileDir $hostAddress`:$hostHome 
 
-$hostInstallSh = "cd $hostVSCDir ; tar -zxf $downFileName ; rm -rf $downFileName ; mv vscode-server-linux-x64/ $hostInstallPath/"
+$hostInstallSh = "cd $hostVSCDir ; tar -zxf $downFileName ; rm -rf $downFileName ; rm -rf $hostInstallPath ; mv vscode-server-linux-x64/ $hostInstallPath/"
 
 ssh -p $hostPort $hostAddress $hostInstallSh
